84d879d71077f44bceeb2860176ac7a1998e0dfc
Phuc-2kar <nguyen.trong.phuc@sun-asterisk.com>
2023-10-01 13:20:48 +0700

    add user to dashboard

--- app/Http/Services/DashboardService.php
+++ app/Http/Services/DashboardService.php
@@ -4,18 +4,21 @@ namespace App\Http\Services;
 use App\Http\Contracts\Repositories\BlogRepositoryContract;
 use App\Http\Contracts\Repositories\CategoryRepositoryContract;
 use App\Http\Contracts\Repositories\RoomRepositoryContract;
+use App\Http\Contracts\Repositories\UserRepositoryContract;
 
 class DashboardService
 {
     protected $blogRepository;
     protected $categoryRepository;
     protected $roomRepository;
+    protected $userRepository;
 
-    public function __construct(BlogRepositoryContract $blogRepository, CategoryRepositoryContract $categoryRepository, RoomRepositoryContract $roomRepository)
+    public function __construct(BlogRepositoryContract $blogRepository, CategoryRepositoryContract $categoryRepository, RoomRepositoryContract $roomRepository, UserRepositoryContract $userRepository)
     {
         $this->blogRepository = $blogRepository;
         $this->categoryRepository = $categoryRepository;
         $this->roomRepository = $roomRepository;
+        $this->userRepository = $userRepository;
     }
 
     public function analyst()
@@ -23,7 +26,7 @@ class DashboardService
         $totalBlog = $this->blogRepository->count();
         $totalCategory = $this->categoryRepository->count();
         $totalRoom = $this->roomRepository->count();
-
-        return compact('totalBlog', 'totalCategory', 'totalRoom');
+        $totalUser = $this->userRepository->count();
+        return compact('totalBlog', 'totalCategory', 'totalRoom', 'totalUser');
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -17,7 +17,7 @@
               <div class="icon">
                 <i class="ion ion-bag"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{route('blogs.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -33,7 +33,7 @@
               <div class="icon">
                 <i class="ion ion-stats-bars"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{route('categories.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -48,24 +48,25 @@
               <div class="icon">
                 <i class="ion ion-person-add"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="{{route('rooms.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
-          <!-- <div class="col-lg-3 col-6">
+          @if (Auth::user()->isAdmin())
+            <div class="col-lg-3 col-6">
+              <div class="small-box bg-danger">
+                <div class="inner">
+                  <h3>{{$data['totalUser']}}</h3>
 
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h3>65</h3>
-
-                <p>Unique Visitors</p>
+                  <p>Users</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="{{route('users.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
               </div>
-              <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-          </div> -->
+          @endif
           <!-- ./col -->
         </div>
 @endsection
